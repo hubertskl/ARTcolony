efebb33498bae3f248f7e68ed66d6985c377834b
szal03 <33811775+szal03@users.noreply.github.com>
2019-10-23 00:46:20 +0200

    fix login

--- src/webApp/registration/registration.php
+++ src/webApp/registration/registration.php
@@ -1,11 +1,8 @@
 <?php
-
 	SESSION_START();
-	
 	if(isset($_POST['email']))
 	{
 		$allright=true;
-		
 			if(isset($_POST['name']))
 			{
 				$name=$_POST['name'];
@@ -15,13 +12,11 @@
 				$surname=$_POST['family_name'];
 			}
 		$username=$_POST['username'];
-
 		if((strlen($username)<3)||(strlen($username)>25))
 		{
 			$allright=false;
 			$_SESSION['e_username']="Login must have more chars";
 		}
-		
 		if(ctype_alnum($username)==false)
 		{
 			$allright=false;
@@ -36,10 +31,8 @@
 			$allright=false;
 			$_SESSION['e_email']="Invalid email";
 		}
-		
 		$password1=$_POST['password1'];
 		$password2=$_POST['password2'];
-		
 		if((strlen($password1)<3)||(strlen($password1)>25))
 		{
 			$allright=false;
@@ -50,27 +43,18 @@
 			$allright=false;
 			$_SESSION['e_password2']="Passwords do not match";
 		}
-		
-		$passwordhash = password_hash($password1, PASSWORD_DEFAULT); 
-		
-		
+		$passwordhash = password_hash($password1, PASSWORD_DEFAULT);
 		if(!isset($_POST['conditions']))
 		{
 			$allright=false;
 			$_SESSION['e_conditions']="You must accept the regulations";
 		}
-		
-		
-		
 			try{
 				require_once '../../connection/connectWithDB.php';
-				
 				$qu1=$db->prepare('SELECT id_user FROM users WHERE email=:email1');
 				$qu1->bindValue(':email1', $email, PDO::PARAM_STR);
 				$qu1->execute();
-		
 				$mailNumber=$qu1->rowCount();
-				
 				if($mailNumber>0)
 				{
 					$allright=false;
@@ -89,58 +73,39 @@
 				}
 				if($allright==true)
 				{
-					
-					$result5=$db->prepare("INSERT INTO users (login, password, name, family_name, email) VALUES (:username,:password,:name,:family_name,:email)"); 
-					
+					$result5=$db->prepare("INSERT INTO users (login, password, name, family_name, email) VALUES (:username,:password,:name,:family_name,:email)");
 					$result5->bindParam(':username',$username);
 					$result5->bindParam(':password',$passwordhash);
 					$result5->bindParam(':name',$name);
 					$result5->bindParam(':family_name',$surname);
 					$result5->bindParam(':email',$email);
-					
-					
-					
 					$result5->execute();
-					
-					
 						$_SESSION['successful_registration']=true;
-						
 						header('Location: afterRegistration.php');
-				
 				}
-			
 		}
 		catch(Exception $e)
 		{
 			echo '<span style="color:red;">Server ERROR!</span>';
 			echo '<br />Info: '.$e;
 		}
-			
-		
 	}
-
 ?>
-
-
 <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
 	<title>ARTcolony</title>
-	<link rel="stylesheet" type = "text/css" href="css/fontello.css">
+	<link rel="stylesheet" type = "text/css" href="registrationStyle/registrationStyle.css">
 
 </head>
 <body>
+		<div id="page-title"><a  href="../index.html">Home</a><br><br></div>
 	<div id="container">
-		<div id="page-title"><a  href="../index.html">Home</a><br><br>
-			<a href="../login/login.php">Login</a>
-		</div>
-		
-			<div id="logo"><h1>Register</h1></div>
+
 				<div id="panel"> 
 					<form method="POST">
-					<p><label for="username">Login:</label></p>
-					<input type="text" id="username" name="username">
+					<input type="text" id="username" name="username" placeholder="USERNAME"></br></br>
 					<?php
 						if(isset($_SESSION['e_username']))
 						{
@@ -148,12 +113,9 @@
 							unset($_SESSION['e_username']);
 						}
 					?>
-					<p>Name: </p>
-					<input type="text" id="name1" name="name">
-					<p>Family name: </p>
-					<input type="text" id="family_name" name="family name">
-					<p>e-mail: </p>
-					<input type="text" id="email" name="email">
+					<input type="text" id="name1" name="name" placeholder="NAME"></br></br>
+					<input type="text" id="family_name" name="family name" placeholder="FAMILY NAME"></br></br>
+					<input type="text" id="email" name="email" placeholder="E-MAIL"></br></br>
 					<?php
 						if(isset($_SESSION['e_email']))
 						{
@@ -161,8 +123,7 @@
 							unset($_SESSION['e_email']);
 						}
 					?>
-					<p><label for="password">Password:</label></p>
-					<input type="password" id="password1" name="password1">
+					<input type="password" id="password1" name="password1" placeholder="PASSWORD"></br></br>
 					<?php
 						if(isset($_SESSION['e_password1']))
 						{
@@ -170,8 +131,7 @@
 							unset($_SESSION['e_password1']);
 						}
 					?>
-					<p><label for="password">Repeat password:</label></p>
-					<input type="password" id="password2" name="password2"></br></br>
+					<input type="password" id="password2" name="password2" placeholder="REPEAT PASSWORD"></br></br>
 					<?php
 						if(isset($_SESSION['e_password2']))
 						{
@@ -180,8 +140,8 @@
 						}
 					?>
 					<label>
-					<input type="checkbox" name="conditions" /> I accept the conditions
-					</label>
+					<input type="checkbox" name="conditions" placeholder="I accept the conditions"/> I accept the conditions</label>
+					<div class="login_button"><input type="submit" value=""></div>
 					<?php
 						if(isset($_SESSION['e_conditions']))
 						{
@@ -189,7 +149,8 @@
 							unset($_SESSION['e_conditions']);
 						}
 					?>
-					<div class="login-button"><p><input type="submit" value="Register"></p></div>
+					
+								<a href="../login/login.php">Already have an account? Sign in!</a>
 					</form>
 				</div>
 	
